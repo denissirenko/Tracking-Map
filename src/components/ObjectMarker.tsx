74465f216962c7b227139112916edd7509c9b87e
denissirenko <sirenko.denis86@gmail.com>
2024-10-17 17:51:52 +0300

    update url marker

--- src/components/ObjectMarker.tsx
+++ src/components/ObjectMarker.tsx
@@ -3,16 +3,14 @@ import { Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import { ObjectType } from "../stores/objectStore";
 
-import marker from "../../public/images/marker-icon.png";
-
 interface ObjectMarkerProps {
   object: ObjectType;
 }
 
 export const ObjectMarker: React.FC<ObjectMarkerProps> = ({ object }) => {
   const icon = new L.Icon({
-    iconUrl: marker,
-    iconSize: [20, 25],
+    iconUrl: "/images/marker.png",
+    iconSize: [18, 25],
   });
 
   return (
